327944b69f46a6065bebe0146a7c3aea7f107431
Ongarr <44764151+Ongarr@users.noreply.github.com>
2019-10-04 21:06:14 +0200

    Add files via upload

---
+++ config/db_connect.php
@@ -0,0 +1,11 @@
+<?php 
+
+    // conntect to database
+    $conn = mysqli_connect('localhost', 'shaun', 'test1234', 'ninja_pizza');
+
+    // check connection
+    if(!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+?>
